8fe420a5004339f2e7eebf0e957b9e5bb7ef3783
kamruzzaman22874 <joybd.eee@gmail.com>
2023-06-07 21:46:45 +0600

    post users

--- index.js
+++ index.js
@@ -27,8 +27,24 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
+        const usersCollection = client.db("danceStudio").collection("users");
 
-        
+        app.get('/users', async(req, res) => {
+            const result = await usersCollection.find().toArray();
+            res.send(result)
+        })
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email }
+            const existingUser = await usersCollection.findOne(query);
+            // console.log(existingUser);
+            if (existingUser) {
+                return res.send({ message: 'user already exists' })
+            }
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
 
 
 
